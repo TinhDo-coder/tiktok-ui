1b4ac45b6aa85b32cccf8d587941bd145b5f1a6b
TinhDo-coder <105311577+TinhDo-coder@users.noreply.github.com>
2022-10-21 03:49:25 +0700

    Build base project tiktok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1> hello ae F8 nhé g</h1>
-    </div>
-  );
-}
+    return (
+        <Router>
+            <div className="App">
+                <ul>
+                    <li>
+                        <Link to={'/'}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={'/following'}>Following</Link>
+                    </li>
+                    <li>
+                        <Link to={'/profile'}>Profile</Link>
+                    </li>
+                    <li>
+                        <Link to={'/upload'}>Upload</Link>
+                    </li>
+                </ul>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
 
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
+}
 export default App;
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,4 @@
+function Upload() {
+    return <h2>Upload pages</h2>;
+}
+export default Upload;
